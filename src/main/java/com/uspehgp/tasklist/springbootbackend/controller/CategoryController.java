5432b040aa363f6637473a86dc27e149f36712a9
uspehgp <z995959z>
2020-08-19 19:09:58 +0300

    add checking param

--- src/main/java/com/uspehgp/tasklist/springbootbackend/controller/CategoryController.java
+++ src/main/java/com/uspehgp/tasklist/springbootbackend/controller/CategoryController.java
@@ -1,7 +1,10 @@
 package com.uspehgp.tasklist.springbootbackend.controller;
 
 import com.uspehgp.tasklist.springbootbackend.entity.Category;
+import com.uspehgp.tasklist.springbootbackend.entity.Priority;
 import com.uspehgp.tasklist.springbootbackend.repo.CategoryRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,7 +27,37 @@ public class CategoryController {
         return list;
     }
     @PostMapping("/add")
-    public Category add(@RequestBody Category category){
-        return categoryRepository.save(category);
+    public ResponseEntity<Category> add(@RequestBody Category category){
+
+        // проверка на обязательные параметры
+        if (category.getId() != null && category.getId() != 0) {
+            // id создается автоматически в БД (autoincrement), поэтому его передавать не нужно, иначе может быть конфликт уникальности значения
+            return new ResponseEntity("redundant param: id MUST be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        // если передали пустое значение title
+        if (category.getTitle() == null || category.getTitle().trim().length() == 0) {
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+
+        return ResponseEntity.ok(categoryRepository.save(category));
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity update(@RequestBody Category category){
+
+        // проверка на обязательные параметры
+        if (category.getId() == null && category.getId() == 0) {
+            // id создается автоматически в БД (autoincrement), поэтому его передавать нужно только при обновлении, иначе может быть конфликт уникальности значения
+            return new ResponseEntity("missed param: id", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        // если передали пустое значение title
+        if (category.getTitle() == null || category.getTitle().trim().length() == 0) {
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return ResponseEntity.ok(categoryRepository.save(category));
     }
 }
--- src/main/java/com/uspehgp/tasklist/springbootbackend/controller/PriorityController.java
+++ src/main/java/com/uspehgp/tasklist/springbootbackend/controller/PriorityController.java
@@ -1,8 +1,9 @@
 package com.uspehgp.tasklist.springbootbackend.controller;
 
-import com.uspehgp.tasklist.springbootbackend.entity.Category;
 import com.uspehgp.tasklist.springbootbackend.entity.Priority;
 import com.uspehgp.tasklist.springbootbackend.repo.PriorityRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,7 +27,42 @@ public class PriorityController {
 
     }
     @PostMapping("/add")
-    public Priority add(@RequestBody Priority priority){
-        return priorityRepository.save(priority);
+    public ResponseEntity<Priority> add(@RequestBody Priority priority){
+
+        // проверка на обязательные параметры
+        if (priority.getId() != null && priority.getId() != 0) {
+            // id создается автоматически в БД (autoincrement), поэтому его передавать не нужно, иначе может быть конфликт уникальности значения
+            return new ResponseEntity("redundant param: id MUST be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        // если передали пустое значение title
+        if (priority.getTitle() == null || priority.getTitle().trim().length() == 0) {
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+
+        return ResponseEntity.ok(priorityRepository.save(priority));
+    }
+    @PutMapping("/update")
+    public ResponseEntity update(@RequestBody Priority priority){
+
+        // проверка на обязательные параметры
+        if (priority.getId() == null && priority.getId() == 0) {
+            // id создается автоматически в БД (autoincrement), поэтому его передавать нужно только при обновлении, иначе может быть конфликт уникальности значения
+            return new ResponseEntity("missed param: id", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        // если передали пустое значение title
+        if (priority.getTitle() == null || priority.getTitle().trim().length() == 0) {
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        // если передали пустое значение color
+        if (priority.getColor() == null || priority.getColor().trim().length() == 0) {
+            return new ResponseEntity("missed param: Color", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+
+        return ResponseEntity.ok(priorityRepository.save(priority));
     }
 }
